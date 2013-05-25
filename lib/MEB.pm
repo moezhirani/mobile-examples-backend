32b2abe883f09f9529e41f4b9d4dd4f022796ff4
Gabor Szabo <szabgab@gmail.com>
2013-05-25 08:32:10 +0300

    add time

--- lib/MEB.pm
+++ lib/MEB.pm
@@ -8,15 +8,15 @@ get '/' => sub {
 };
 
 get '/echo' => sub {
-	return 'Echo GET ' . param('txt');
+	return 'Echo GET ' . param('txt') . ' at ' . localtime;
 };
 
 get '/echo.json' => sub {
-	return to_json { method => 'GET', txt => param('txt') };
+	return to_json { method => 'GET', txt => param('txt'), 'time' => scalar localtime };
 };
 
 post '/echo.json' => sub {
-	return to_json { method => 'POST', txt => param('txt') };
+	return to_json { method => 'POST', txt => param('txt'), 'time' => scalar localtime };
 };
 
 
